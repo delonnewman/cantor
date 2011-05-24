c3d465ad7f3e369f56004085f054255eec675b78
Delon Newman <drnewman@phrei.org>
2011-05-24 12:30:34 -1000

    playing with inspect

--- lib/cantor/set.rb
+++ lib/cantor/set.rb
@@ -62,7 +62,7 @@ module Cantor
 
 		def inspect
 			"#<#{self.class.inspect} @superset=#{@superset.inspect} " +
-			"@set= @subsets=#{@subsets.inspect}>"
+			"@members=#{@members.inspect}>"
 		end
 
 		def eval
